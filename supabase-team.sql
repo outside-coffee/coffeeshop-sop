6e051f83ac4551e114f5a6aef884b6f289feb183
alaya9 <hb.manaa.alaya@gmail.com>
2026-05-25 18:26:20 +0200

    ignore sql

--- supabase-team.sql
+++
@@ -1,7 +0,0 @@
--- Ajouter colonnes dans profiles
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS prenom       text;
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS nom          text;
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS role_operationnel text;  -- Barista Lead, Barista, Service Crew, Support Crew
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS date_recrutement date;
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS telephone    text;
-ALTER TABLE profiles ADD COLUMN IF NOT EXISTS actif        boolean DEFAULT true;
